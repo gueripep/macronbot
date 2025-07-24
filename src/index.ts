eddcd26b7302176f3de319dadc00aa60120530d6
Pouks <poulbleu.du38@gmail.com>
2025-07-24 11:34:52 +0200

    test webhook i believe

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { commands, rest } from "./deploy-command.js";
 import { handleMessage } from "./handlers/messageHandler.js";
 import { getPortfolioEmbed, handleTradeCommand } from "./services/macron-trade/macron-trade-service.js";
 import { RememberService } from "./services/remember-service.js";
-import { scheduleDailyTasks, sendDailyMessage } from "./services/scheduler.js";
+import { scheduleDailyTasks } from "./services/scheduler.js";
 
 
 const client = new Client({
@@ -20,7 +20,6 @@ client.once("ready", (): void => {
   console.log(`Logged in as ${client.user?.tag}`);
   try {
     scheduleDailyTasks(client);
-    sendDailyMessage(client);
   } catch (error) {
     console.error("Error scheduling daily messages:", error);
   }
